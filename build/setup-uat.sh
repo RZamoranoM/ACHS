f3582d89b313241542af6b590b5c5a9fe4b5cfa1
Karen Rodríguez <84875960+Karenrodriguezlab@users.noreply.github.com>
2021-08-31 14:59:07 -0400

    Update setup-uat.sh

--- build/setup-uat.sh
+++ build/setup-uat.sh
@@ -2,9 +2,9 @@
 echo "Setting up DevHub Connection..."
 echo "Authenticating org"
 
-echo "Conectar Desa"
-sfdx alias:set my-hub-orgDESA=consultor_force@ei.cl.sbxei
-sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
+#echo "Conectar Desa"
+#sfdx alias:set my-hub-orgDESA=consultor_force@ei.cl.sbxei
+#sfdx auth:jwt:grant --clientid $SFDC_DESA_CLIENTID --jwtkeyfile JWT/server.key --username $SFDC_DESA_USER --instanceurl https://test.salesforce.com
 
 echo "Conectar QA"
 sfdx alias:set my-hub-orgQA=consultor_force@nectia.com.qa
